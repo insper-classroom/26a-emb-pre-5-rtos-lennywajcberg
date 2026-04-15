28284af9afae4b337ecbc84e3e186164a6c0e205
lennywajcberg <lennyw@al.insper.edu.br>
2026-04-15 18:15:47 -0300

    Ex2

--- exe2/main.c
+++ exe2/main.c
@@ -31,6 +31,23 @@ void led_1_task(void *p) {
   }
 }
 
+void led_2_task(void *p) {
+  gpio_init(LED_PIN_G);
+  gpio_set_dir(LED_PIN_G, GPIO_OUT);
+
+  int delay = 250;
+
+  while (true) {
+
+    if (xSemaphoreTake(xSemaphore_r, pdMS_TO_TICKS(500)) == pdTRUE) { 
+      gpio_put(LED_PIN_G, 1);
+      vTaskDelay(pdMS_TO_TICKS(delay));
+      gpio_put(LED_PIN_G, 0);
+      vTaskDelay(pdMS_TO_TICKS(delay));
+    }
+  }
+}
+
 void btn_1_task(void *p) {
   gpio_init(BTN_PIN_R);
   gpio_set_dir(BTN_PIN_R, GPIO_IN);
@@ -46,6 +63,22 @@ void btn_1_task(void *p) {
   }
 }
 
+void btn_2_task(void *p) {
+  gpio_init(BTN_PIN_G);
+  gpio_set_dir(BTN_PIN_G, GPIO_IN);
+  gpio_pull_up(BTN_PIN_G);
+
+  while (true) {
+    if (!gpio_get(BTN_PIN_G)) {
+      while (!gpio_get(BTN_PIN_G)) {
+        vTaskDelay(pdMS_TO_TICKS(1));
+      }
+      xSemaphoreGive(xSemaphore_r);
+    }
+  }
+}
+
+
 int main() {
   stdio_init_all();
   printf("Start RTOS \n");
@@ -54,6 +87,9 @@ int main() {
 
   xTaskCreate(led_1_task, "LED_Task 1", 256, NULL, 1, NULL);
   xTaskCreate(btn_1_task, "BTN_Task 1", 256, NULL, 1, NULL);
+  xTaskCreate(led_2_task, "LED_Task 2", 256, NULL, 1, NULL);
+  xTaskCreate(btn_2_task, "BTN_Task 2", 256, NULL, 1, NULL);
+
 
   vTaskStartScheduler();
 
