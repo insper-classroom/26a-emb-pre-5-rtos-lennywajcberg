79104a5dc83b9c64eaeb0d1b1262a6ae17649623
lennywajcberg <lennyw@al.insper.edu.br>
2026-04-15 18:18:02 -0300

    Ex2

--- exe2/main.c
+++ exe2/main.c
@@ -86,6 +86,7 @@ int main() {
   printf("Start RTOS \n");
 
   xSemaphore_r = xSemaphoreCreateBinary();
+  xSemaphore_g = xSemaphoreCreateBinary();
 
   xTaskCreate(led_1_task, "LED_Task 1", 256, NULL, 1, NULL);
   xTaskCreate(btn_1_task, "BTN_Task 1", 256, NULL, 1, NULL);
