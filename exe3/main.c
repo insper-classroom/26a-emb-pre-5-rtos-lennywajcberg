61651443cd78a4cd8d5fe816ea0ef118764183f1
lennywajcberg <lennyw@al.insper.edu.br>
2026-04-15 18:45:45 -0300

    Ex2

--- exe3/main.c
+++ exe3/main.c
@@ -12,15 +12,16 @@ const int BTN_PIN_G = 26;
 const int LED_PIN_R = 4;
 const int LED_PIN_G = 6;
 
-QueueHandle_t xQueueButId;
+QueueHandle_t xQueueButId; // fila simplesmente é uma estrutura de dados que armazena elementos em uma ordem específica, permitindo a comunicação entre tarefas em um sistema operacional de tempo real (RTOS). Ela é usada para enviar dados de uma tarefa para outra, garantindo a sincronização e a troca de informações de forma segura.
+QueueHandle_t xQueueVerde; 
 
-void led_1_task(void *p) {
+void led_1_task(void *p) { 
     gpio_init(LED_PIN_R);
     gpio_set_dir(LED_PIN_R, GPIO_OUT);
 
-    int delay = 0;
+    int delay = 0; // variável para armazenar o valor do atraso recebido da fila. Ela é usada para controlar o tempo de atraso entre as ações de ligar e desligar o LED. O valor do atraso é atualizado com base nas mensagens recebidas da fila, permitindo que a tarefa de controle do LED ajuste seu comportamento dinamicamente.    
     while (true) {
-        if (xQueueReceive(xQueueButId, &delay, 0)) {
+        if (xQueueReceive(xQueueButId, &delay, 0)) { // 
             printf("%d\n", delay);
         }
 
@@ -28,6 +29,25 @@ void led_1_task(void *p) {
             gpio_put(LED_PIN_R, 1);
             vTaskDelay(pdMS_TO_TICKS(delay));
             gpio_put(LED_PIN_R, 0);
+            vTaskDelay(pdMS_TO_TICKS(delay));
+        }
+    }
+}
+
+void led_2_task(void *p) {
+    gpio_init(LED_PIN_G);
+    gpio_set_dir(LED_PIN_G, GPIO_OUT);
+
+    int delay = 0; 
+        while (true) {
+        if (xQueueReceive(xQueueVerde, &delay, 0)) { // 
+            printf("%d\n", delay);
+        }
+
+        if (delay > 0) {
+            gpio_put(LED_PIN_G, 1);
+            vTaskDelay(pdMS_TO_TICKS(delay));
+            gpio_put(LED_PIN_G, 0);
             vTaskDelay(pdMS_TO_TICKS(delay));
         }
     }
@@ -58,14 +78,42 @@ void btn_1_task(void *p) {
 }
 
 
+void btn_2_task(void *p) {
+    gpio_init(BTN_PIN_G);
+    gpio_set_dir(BTN_PIN_G, GPIO_IN);
+    gpio_pull_up(BTN_PIN_G);
+
+    int delay = 0;
+    while (true) {
+        if (!gpio_get(BTN_PIN_G)) {
+
+            while (!gpio_get(BTN_PIN_G)) {
+                vTaskDelay(pdMS_TO_TICKS(1));
+            }
+
+            if (delay < 1000) {
+                delay += 100;
+            } else {
+                delay = 100;
+            }
+            printf("delay btn %d \n", delay);
+            xQueueSend(xQueueVerde, &delay, 0);
+        }
+    }
+}
+
+
 int main() {
     stdio_init_all();
     printf("Start RTOS \n");
 
     xQueueButId = xQueueCreate(32, sizeof(int));
+    xQueueVerde = xQueueCreate(32, sizeof(int));
 
     xTaskCreate(led_1_task, "LED_Task 1", 256, NULL, 1, NULL);
     xTaskCreate(btn_1_task, "BTN_Task 1", 256, NULL, 1, NULL);
+    xTaskCreate(led_2_task, "LED_Task 2", 256, NULL, 1, NULL);
+    xTaskCreate(btn_2_task, "BTN_Task 2", 256, NULL, 1, NULL);
 
     vTaskStartScheduler();
 
