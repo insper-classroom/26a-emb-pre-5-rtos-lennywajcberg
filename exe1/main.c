fdeb03e93becae890f1459c80b29e213dcaf34e2
lennywajcberg <lennyw@al.insper.edu.br>
2026-04-15 18:08:01 -0300

    Ex1

--- exe1/main.c
+++ exe1/main.c
@@ -23,10 +23,24 @@ void led_1_task(void *p) {
   }
 }
 
+void led_2_task(void *p) {
+  gpio_init(LED_PIN_G); // inicio o led
+  gpio_set_dir(LED_PIN_G, GPIO_OUT); // aponto o led como saida
+
+  int delay = 250; // dou um delay de 250 ms
+  while (true) {
+    gpio_put(LED_PIN_G, 1); // acendo o led, dou um delay, desligo e dou um delay
+    vTaskDelay(pdMS_TO_TICKS(delay));
+    gpio_put(LED_PIN_G, 0);
+    vTaskDelay(pdMS_TO_TICKS(delay));
+  }
+}
+
 int main() {
   stdio_init_all();
   printf("Start RTOS \n");
-  xTaskCreate(led_1_task, "LED_Task 1", 256, NULL, 1, NULL);
+  xTaskCreate(led_1_task, "LED_Task 1", 256, NULL, 1, NULL); // o quarto arguemtno é o *p =  NULL, n aponta pra lugar nenhum
+  xTaskCreate(led_2_task, "LED_Task 2", 256, NULL, 1, NULL);
   vTaskStartScheduler();
 
   while (true)
